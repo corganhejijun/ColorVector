623bcfbdf8683e5bbd2b27881c57c75fd8445432
corgan hejijun <corganhejijun@gmail.com>
2016-04-17 23:47:00 +0800

    combine regions

--- ColorContourTest.m
+++ ColorContourTest.m
@@ -4,7 +4,8 @@ fileName = '12003.jpg';
 fig = imread(fileName);
 colorDistance = ColorContour(fig);
 [clusterResult, regions] = ClusterColorContour(colorDistance);
-
-% for i = 1 : size(weightOutline, 3)
-%     figure;imagesc(weightOutline(:,:,i));
-% end
+combineRegions = CombineRegions(regions);
+%%
+for i = 1 : size(regions, 3)
+    figure;imagesc(regions(:,:,i));
+end
