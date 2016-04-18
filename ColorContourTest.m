5c59e3ee1d13f9abbf015480c7eef5ac36c667ff
corgan hejijun <corganhejijun@gmail.com>
2016-04-18 16:34:19 +0800

    get combine regions

--- ColorContourTest.m
+++ ColorContourTest.m
@@ -1,11 +1,11 @@
 %% 
 clear; close all; clc;
-fileName = '12003.jpg';
+fileName = '253027.jpg';
 fig = imread(fileName);
 colorDistance = ColorContour(fig);
 [clusterResult, regions] = ClusterColorContour(colorDistance);
 combineRegions = CombineRegions(regions);
 %%
-for i = 1 : size(regions, 3)
-    figure;imagesc(regions(:,:,i));
+for i = 1 : size(combineRegions, 3)
+    figure;imagesc(combineRegions(:,:,i));
 end
--- CombineRegions.m
+++ CombineRegions.m
@@ -6,8 +6,9 @@ function [ result ] = CombineRegions( regions )
     backgroundHoleThres = height*width/100;
     regionList = {};
     regionSizeList = [];
+    tic;
     for i = 1 : channelCnt
-        region = regions(i);
+        region = regions(:,:,i);
 
         % regions in one channel is labeled
         regionNo = max(region(:));
@@ -15,7 +16,7 @@ function [ result ] = CombineRegions( regions )
             region1 = (region == j);
 
             % if region contain large holes, it would be background region
-            regionFill = imfill(region1);
+            regionFill = imfill(region1, 'holes');
             holeDiff = xor(region1, regionFill);
             if (sum(holeDiff(:)) > backgroundHoleThres)
                 continue;
@@ -24,6 +25,7 @@ function [ result ] = CombineRegions( regions )
             found = false;
             for k = 1 : length(regionList)
                 region2 = regionList{k};
+                % regions with small different will be merge to the bigger one
                 regionDiff = xor(region2, region1);
                 if sum(regionDiff(:)) < diffThreshold
                     found = true;
@@ -43,27 +45,38 @@ function [ result ] = CombineRegions( regions )
             end
         end
     end
+    t = toc;
+    fprintf('seprate regions done: %1.2f sec\n', t);
 
     % sort region list from small to big
-    sortList = [(1:length(regionList))', regionSizeList];
+    sortList = [(1:length(regionList))', regionSizeList'];
     sortList = sortrows(sortList, 2);
 
-    result = zeros(height, width);
+    result = [];
+    tic;
     for i = 1 : length(regionList)
         region = regionList{sortList(i, 1)};
         intersect = true;
         for j = 1 : size(result, 3)
+            if isempty(result)
+                break;
+            end
             region2 = result(:, :, j);
-            regionDiff = xor(region, region2);
-            if sum(regionDiff(:)) == 0
+            % combine not intersect regions into one channel
+            regionIntersect = and(region, region2);
+            if sum(regionIntersect(:)) == 0
                 region2 = or(region2, region);
                 result(:,:,j) = region2 + result(:,:,j);
                 intersect = false;
                 break;
             end
         end
-        if intersect
+        if isempty(result)
+            result(:,:,1) = region;
+        elseif intersect
             result(:,:,size(result, 3) + 1) = region;
         end
     end
+    t = toc;
+    fprintf('combine regions done: %1.2f sec\n', t);
 end
